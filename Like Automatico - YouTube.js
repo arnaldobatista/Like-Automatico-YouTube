43bede614023bd9d248cb85e98cf3a62e0e821a8
Arnaldo Carpi <51098299+arnaldocarpi@users.noreply.github.com>
2022-10-28 12:41:12 -0300

    General code refactoring and button like correction

--- Like Automatico - YouTube.js
+++ Like Automatico - YouTube.js
@@ -2,7 +2,7 @@
 // @name         Like Automático e Download - YouTube
 // @homepageURL
 // @namespace    http://tampermonkey.net/
-// @version      4.1
+// @version      5.0
 // @description  Script para curtir automaticamente vídeos do Youtube e add botão de dowload
 // @license MIT
 // @icon https://logospng.org/download/facebook-like/logo-facebook-like-1536.png
@@ -15,31 +15,28 @@
 
 (function () {
     'use strict'
+    // like automatico
     const like = () => {
         const interval = setInterval(() => {
-            const btnlike = document.querySelector('ytd-toggle-button-renderer > yt-button-shape > button')
-            if (btnlike) {
-                clearInterval(interval)
-    
-                if(btnlike.getAttribute("aria-pressed") == 'false'){
-                    btnlike.click()
+            const btnLike = document.querySelector('ytd-toggle-button-renderer > yt-button-shape > button')
+            const btnDesLike = document.querySelector('ytd-segmented-like-dislike-button-renderer > div:nth-child(2) > ytd-toggle-button-renderer > yt-button-shape > button')
+
+            if (btnLike) {
+                if (btnDesLike.getAttribute("aria-pressed") == btnLike.getAttribute("aria-pressed")) {
+                    btnLike.click()
+                    clearInterval(interval)
+                    return
+                } else {
+                    clearInterval(interval)
+                    return
                 }
             }
         }, 1000)
     }
-    document.body.addEventListener('yt-navigate-finish', function () {
-        if (window.location.pathname == '/watch') {
-            like()
-        }
-    })
+    // fim - like automatico
 
-    const waitForElement = (selector, callback) => {
-        const element = document.querySelector(selector)
-        if (element) return callback(element)
-        return window.requestAnimationFrame(() => waitForElement(selector, callback))
-    }
-
-    const downloadButton = document.createElement( 'tp-yt-paper-button' )
+    // btn download
+    const downloadButton = document.createElement('tp-yt-paper-button')
     downloadButton.textContent = 'Download'
     downloadButton.classList.add('yt-spec-button-shape-next')
     downloadButton.classList.add('yt-spec-button-shape-next--tonal')
@@ -47,22 +44,37 @@
     downloadButton.classList.add('yt-spec-button-shape-next--size-m')
     downloadButton.onclick = () => window.open(`https://www.ssyoutube.com/watch?v=${new URL(window.location).searchParams.get("v")}`, '_blank')
 
-    setInterval(() => {
-        if (document.querySelector( 'ytd-subscribe-button-renderer > yt-button-shape > button > div > span').innerHTML === 'Inscrever-se') {
-            document.querySelector( 'ytd-subscribe-button-renderer > tp-yt-paper-button:nth-child(5)' ).setAttribute('style', 'margin-left: 10px;')
-            clearInterval()
+    const waitForElement = (selector, callback) => {
+        const element = document.querySelector(selector)
+        if (element) return callback(element)
+        return window.requestAnimationFrame(() => waitForElement(selector, callback))
+    }
+
+    const styleMarginLeftDownload = setInterval(() => {
+        const subscription = document.querySelector('ytd-subscribe-button-renderer > yt-button-shape > button > div > span').innerHTML
+        if (subscription === 'Inscrever-se') {
+            document.querySelector('ytd-subscribe-button-renderer > tp-yt-paper-button:nth-child(5)').setAttribute('style', 'margin-left: 10px;')
+        } if (subscription === 'Inscrito') {
+            document.querySelector('ytd-subscribe-button-renderer > tp-yt-paper-button:nth-child(5)').setAttribute('style')
         }
     }, 1000)
+    // fim - btn download
 
-    waitForElement('ytd-subscribe-button-renderer', target => target.appendChild( downloadButton ))
-
+    // remover btn download YT premium
     setInterval(() => {
-        if(document.querySelector('ytd-download-button-renderer')){
+        if (document.querySelector('ytd-download-button-renderer')) {
             const trueDownaload = document.querySelector('ytd-download-button-renderer')
-            if(trueDownaload.parentNode){
+            if (trueDownaload.parentNode) {
                 trueDownaload.parentNode.removeChild(trueDownaload)
                 clearInterval()
             }
         }
     }, 1000)
+    // fim - remover btn download YT premium
+
+    if (window.location.pathname == '/watch') {
+        setInterval(() => like(), 2000)
+        waitForElement('ytd-subscribe-button-renderer', target => target.appendChild(downloadButton))
+        styleMarginLeftDownload()
+    }
 })()
